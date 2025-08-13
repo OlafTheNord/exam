64e54dd2ce50ca107a68193b5a495ebe7b03c589
Артемов Александр <artemov.a23@rwb.ru>
2025-08-13 23:49:01 +0300

    inputs fix

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"exam_basic/calc"
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var (
 		firstNum  int
 		secondNum int
 		operation string
+		result    float64
 	)
+
 	fmt.Println("Input a first number: ")
 	_, err := fmt.Scan(&firstNum)
 	if err != nil {
@@ -22,12 +28,33 @@ func main() {
 		return
 	}
 
-	fmt.Println("Input type of operation (+, -, *, /, avg, history, undo): ")
-	_, err = fmt.Scan(&operation)
-	if err != nil {
-		fmt.Printf("Error: %q\n", err)
-		return
+	for strings.ToLower(strings.Trim(operation, " ")) != "stop" {
+		fmt.Println("Input type of operation (+, -, *, /, %, avg, history, undo) or \"stop\" to exit: ")
+		fmt.Scan(&operation)
+		switch operation {
+		case "+":
+			result = float64(calc.Sum(firstNum, secondNum))
+			fmt.Printf("Sum is: %v\n", calc.Sum(firstNum, secondNum))
+		case "-":
+			result = float64(calc.Subtract(firstNum, secondNum))
+			fmt.Printf("Subtract is: %v\n", calc.Subtract(firstNum, secondNum))
+		case "*":
+			result = float64(calc.Multiply(firstNum, secondNum))
+			fmt.Printf("Multiple is: %v\n", calc.Multiply(firstNum, secondNum))
+		case "/":
+			result = calc.Divide(firstNum, secondNum)
+			fmt.Printf("Divide is: %v\n", result)
+		case "%":
+			result = calc.DivideWithReminder(firstNum, secondNum)
+			fmt.Printf("Remind from divide is: %v\n", result)
+		case "undo": //TO DO
+			fmt.Println("To do...")
+		case "avg": //TO DO
+			fmt.Println("To do...")
+		case "history": //TO DO
+			fmt.Println("To do...")
+		default:
+			continue
+		}
 	}
-
-	println(firstNum, operation, secondNum)
 }
